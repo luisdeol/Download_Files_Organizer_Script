b674ca9109644496043f02761ff8213f69c48af7
luisfelipekai <luisfelipekai@gmail.com>
2016-11-08 11:04:51 -0300

    Comment the code and group the images in the same folder

--- File_Organizer_Script.py
+++ File_Organizer_Script.py
@@ -4,16 +4,20 @@ import sys
 import shutil
 
 
-
+images_extensions = ["jpeg", "tiff", "bmp", "jpg", "png"]
+#You can change the path below to your current Download's path 
+#(or any path that you want to organize)
 path = "C:\\Users\\luisdeolpy\\Downloads"
 for filename in os.listdir(path):
 	if (os.path.isdir(os.path.join(path,filename))):
 		pass
 	else:
 		ext = filename.split(".")[-1]
-		if not (os.path.exists(os.path.join(path, ext))):
-			os.makedirs(os.path.join(path, ext))
+		#I'm grouping all the images formats under the same unique label "images"
+		if ext in images_extensions:
+			if not (os.path.exists(os.path.join(path, "images"))):
+				os.makedirs(os.path.join(path, "images"))
+			shutil.move(os.path.join(path, filename), os.path.join(path, "images"))
 		else:
-			print("dir already exists")
 			shutil.move(os.path.join(path, filename), os.path.join(path, ext))
 			
