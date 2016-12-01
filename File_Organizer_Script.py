47c2b6d36266b4f0022a282f0d8564c693b80840
luisfelipekai <luisfelipekai@gmail.com>
2016-12-01 14:00:15 -0300

    Correct folder creation

--- File_Organizer_Script.py
+++ File_Organizer_Script.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import shutil
 
 
 images_extensions = ["jpeg", "tiff", "bmp", "jpg", "png"]
-# You can change the path below to your current Download's path
-# (or any path that you want to organize)
 path = "C:\\Users\\luisdeolpy\\Downloads"
+#if not (os.path.exists(os.path.join(path, "mp3"))):
+#	os.makedirs(os.path.join(path, "mp3"))
 for filename in os.listdir(path):
-	if (os.path.isdir(os.path.join(path, filename))):
+	if (os.path.isdir(os.path.join(path,filename))):
 		pass
 	else:
 		ext = filename.split(".")[-1]
-		# I'm grouping all the images formats under the same unique label "images"
 		if ext in images_extensions:
 			if not (os.path.exists(os.path.join(path, "images"))):
 				os.makedirs(os.path.join(path, "images"))
 			shutil.move(os.path.join(path, filename), os.path.join(path, "images"))
+		elif not (os.path.exists(os.path.join(path, ext))):
+			os.makedirs(os.path.join(path, ext))
+			shutil.move(os.path.join(path, filename), os.path.join(path, ext))
 		else:
 			shutil.move(os.path.join(path, filename), os.path.join(path, ext))
+			
