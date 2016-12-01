e350117e0d53845d58ca6f1e217cc0c5799c16ad
luisfelipekai <luisfelipekai@gmail.com>
2016-12-01 14:07:45 -0300

    Solve PEP8 guideline's errors.

--- File_Organizer_Script.py
+++ File_Organizer_Script.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
 import os
-import sys
 import shutil
 
 
 images_extensions = ["jpeg", "tiff", "bmp", "jpg", "png"]
 path = "C:\\Users\\luisdeolpy\\Downloads"
-#if not (os.path.exists(os.path.join(path, "mp3"))):
-#	os.makedirs(os.path.join(path, "mp3"))
 for filename in os.listdir(path):
-	if (os.path.isdir(os.path.join(path,filename))):
-		pass
-	else:
-		ext = filename.split(".")[-1]
-		if ext in images_extensions:
-			if not (os.path.exists(os.path.join(path, "images"))):
-				os.makedirs(os.path.join(path, "images"))
-			shutil.move(os.path.join(path, filename), os.path.join(path, "images"))
-		elif not (os.path.exists(os.path.join(path, ext))):
-			os.makedirs(os.path.join(path, ext))
-			shutil.move(os.path.join(path, filename), os.path.join(path, ext))
-		else:
-			shutil.move(os.path.join(path, filename), os.path.join(path, ext))
-			
+    if (os.path.isdir(os.path.join(path, filename))):
+        pass
+    else:
+        ext = filename.split(".")[-1]
+        if ext in images_extensions:
+            if not (os.path.exists(os.path.join(path, "images"))):
+                os.makedirs(os.path.join(path, "images"))
+                shutil.move(os.path.join(path, filename),
+                            os.path.join(path, "images"))
+        elif not (os.path.exists(os.path.join(path, ext))):
+            os.makedirs(os.path.join(path, ext))
+            shutil.move(os.path.join(path, filename), os.path.join(path, ext))
+        else:
+            shutil.move(os.path.join(path, filename), os.path.join(path, ext))
